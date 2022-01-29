07269d0e4928152c7144a182c279d6085ac664ea
kooskoos20 <omkarbirade20@gmail.com>
2022-01-29 20:59:15 +0530

    feat: support for teams and groups

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const colorIdentifiersForPeople = {
     "Abhinav Manchanda": "#990011FF",
     "Renuka Tamboli": "#76528BFF",
-    "Omkar Birade": "#006B38FF"
+    "Omkar Birade": "#006B38FF",
+    "Interleap Team": "#AFD275FF"
 }
 
 const observer = new MutationObserver(() => {
@@ -9,8 +10,6 @@ const observer = new MutationObserver(() => {
     if(!container) return
     const color = colorIdentifiersForPeople[container.innerText]
     if(!color) return
-    const textElement = document.querySelector(`[data-name='${container.innerText}']`)
-    textElement.style.fontSize = "20px"
     container.style.backgroundColor = color
     observer.disconnect()
 });
